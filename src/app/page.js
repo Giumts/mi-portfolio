68de67eea52af0a6db1ecfd1f4001041699811de
Giumts <giuliat97@hotmail.com>
2026-04-29 10:06:34 +0200

    Añadida nueva sección o cambio de diseño

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,3 @@
-const [isMobile, setIsMobile] = useState(false);
-
-useEffect(() => {
-  const checkMobile = () => setIsMobile(window.innerWidth < 768);
-  checkMobile();
-  window.addEventListener("resize", checkMobile);
-  return () => window.removeEventListener("resize", checkMobile);
-}, []);
-
 "use client";
 import { useState, useEffect, useRef } from "react";
 import ImageTrail from "./ImageTrail";
